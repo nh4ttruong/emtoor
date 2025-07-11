55a1653aa4f1a3846c43009fd0f4d5b151163f59
nh4ttruong <nh4ttruong@users.noreply.github.com>
2025-07-11 07:05:08 +0000

    deploy: 44848c62dd51023f89576f80c03bf1a4f80c9523

--- searcher-6752007c.js
+++ searcher-6752007c.js
@@ -524,6 +524,6 @@ window.search = window.search || {};
         document.head.append(script);
     }
 
-    loadScript(path_to_root + 'searchindex-53139a20.js', 'search-index');
+    loadScript(path_to_root + 'searchindex-4a9f5979.js', 'search-index');
 
 })(window.search);
