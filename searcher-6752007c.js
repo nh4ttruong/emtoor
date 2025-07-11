dad56fe66707350196fccbad3bde9456ca7cd8c1
nh4ttruong <nh4ttruong@users.noreply.github.com>
2025-07-11 06:10:09 +0000

    deploy: a96259a991cb3c6c69ee6ed35ecb0b2abc5c6d23

--- searcher-6752007c.js
+++ searcher-6752007c.js
@@ -524,6 +524,6 @@ window.search = window.search || {};
         document.head.append(script);
     }
 
-    loadScript(path_to_root + 'searchindex-ed568050.js', 'search-index');
+    loadScript(path_to_root + 'searchindex-76ee43a0.js', 'search-index');
 
 })(window.search);
