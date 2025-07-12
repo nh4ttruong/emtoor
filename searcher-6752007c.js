2e25faa31977c7bbf086ec378eb3c1616faa2c63
nh4ttruong <nh4ttruong@users.noreply.github.com>
2025-07-12 14:32:57 +0000

    deploy: e6b2dc6142e6a77fcf4178d8bb11864093e82666

--- searcher-6752007c.js
+++ searcher-6752007c.js
@@ -524,6 +524,6 @@ window.search = window.search || {};
         document.head.append(script);
     }
 
-    loadScript(path_to_root + 'searchindex-4a9f5979.js', 'search-index');
+    loadScript(path_to_root + 'searchindex-b99e6e98.js', 'search-index');
 
 })(window.search);
